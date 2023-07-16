ce34549f34a9afb8918676322061d440ce1d5774
RichardMushunje <124087009+RichardMushunje@users.noreply.github.com>
2023-07-16 07:05:59 -0700

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,52 +1,31 @@
 #!/usr/bin/python3
-
 """
-    Defines a class TestState.
+test_state module
 """
-
-
+from unittest import TestCase
+import pycodestyle
 from models.state import State
-import unittest
-import models
-import os
 
 
-class TestState(unittest.TestCase):
-    """Represent a State."""
+class TestState(TestCase):
+    """
+    TestState class
+    """
 
-    def setUp(self):
-        """SetUp method"""
+    def test_pep(self):
+        """test pep"""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['models/state.py',
+                                    'tests/test_models/test_state.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-        self.state = State()
+    def test_module_doc(self):
+        """test module documentation"""
+        doc = __import__('models.state').__doc__
+        self.assertGreater(len(doc), 1)
 
-    def TearDown(self):
-        """TearDown method."""
-
-        del self.state
-
-    def test_docstring(self):
-        """Test docstring for the module and the class"""
-
-        self.assertIsNotNone(
-            models.state.__doc__,
-            "No docstring in the module"
-        )
-        self.assertIsNotNone(State.__doc__, "No docstring in the class")
-
-    def test_permissions_file(self):
-        """Test File state.py permissions"""
-
-        test_file = os.access("models/state.py", os.R_OK)
-        self.assertTrue(test_file, "Read permissions")
-        test_file = os.access("models/state.py", os.W_OK)
-        self.assertTrue(test_file, "Write Permissions")
-        test_file = os.access("models/state.py", os.X_OK)
-        self.assertTrue(test_file, "Execute permissions")
-
-    def test_type_object(self):
-        """Test type object of State"""
-
-        self.assertEqual(
-            str(type(self.state)),
-            "<class 'models.state.State'>")
-        self.assertIsInstance(self.state, State)
+    def test_class_doc(self):
+        """test class documentation"""
+        doc = State.__doc__
+        self.assertGreater(len(doc), 1)
